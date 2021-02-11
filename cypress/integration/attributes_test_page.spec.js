58d26bbe57a42ab1124c9d6ce62460e8713b2ef9
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-02-11 07:07:49 +0200

    add spec for the attributes test page

---
+++ cypress/integration/attributes_test_page.spec.js
@@ -0,0 +1,21 @@
+import formatUrl from '../utils/formatUrl';
+import pathnames from '../constants/pathnames';
+import * as selectors from '../selectors';
+
+describe('Attributes test page', () => {
+  before('load the attributes test page', () => {
+    cy.visit(formatUrl(pathnames.attributesPage));
+  });
+
+  it('should assert adding of a dynamic attribute by clicking on a button', () => {
+    cy.get(selectors.ATTRIBUTES_TEST_PAGE.PARAGRAPH_WITH_DYNAMIC_ATTRIBUTES)
+      .should('have.attr', 'nextid', 1)
+      .and('not.have.attr', 'custom-1', 'value-1')
+      .and('not.have.attr', 'nextid', 2)
+      .get(selectors.ATTRIBUTES_TEST_PAGE.BUTTON)
+      .click()
+      .get(selectors.ATTRIBUTES_TEST_PAGE.PARAGRAPH_WITH_DYNAMIC_ATTRIBUTES)
+      .should('have.attr', 'nextid', 2)
+      .and('have.attr', 'custom-1', 'value-1');
+  });
+});
