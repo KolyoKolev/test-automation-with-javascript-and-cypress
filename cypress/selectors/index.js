3fcb51944967065baec24768c1e93515f448488a
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-02-11 04:57:41 +0200

    add initial selectors

---
+++ cypress/selectors/index.js
@@ -0,0 +1,8 @@
+export const BASIC_TEST_PAGE = {
+  PAGE_BODY: '.page-body',
+  NAVIGATION_INDEX_LINK: '.page-navigation a',
+  PAGE_H1_HEADING: 'h1',
+  PARAGRAPH: 'p',
+  EXPLANATION_SECTION: '.explanation',
+  FOOTER: '.page-footer',
+};
