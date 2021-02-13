852095f3397f60c48743c94caacdbb2300d98593
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-02-13 12:55:29 +0200

    update the formatUrl function

--- cypress/utils/formatUrl.js
+++ cypress/utils/formatUrl.js
@@ -1,7 +1,14 @@
-const BASE_URL = "https://testpages.herokuapp.com/styled/";
+import { BASE_URL, HTML, PHP } from '../constants';
 
-const formatUrl = (path) => {
-  return `${BASE_URL}${path}.html`;
+const formatUrl = (path, fileType) => {
+  switch (fileType) {
+    case HTML:
+      return `${BASE_URL}${path}.${HTML}`;
+    case PHP:
+      return `${BASE_URL}${path}.${PHP}`;
+    default:
+      return new Error(cy.log('Check the url format. Must be wrong!'));
+  }
 };
 
 export default formatUrl;
