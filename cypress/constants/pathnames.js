acadd698afa4a5748bfc82eec71812b137323b9f
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-02-13 12:56:07 +0200

    add new paths

--- cypress/constants/pathnames.js
+++ cypress/constants/pathnames.js
@@ -3,6 +3,8 @@ const pathnames = {
   basicWebPage: 'basic-web-page-test',
   attributesPage: 'attributes-test',
   tablePage: 'tag/table',
+  basicJavaScriptValidationPage: 'basic-javascript-validation-test',
+  theFormProcessorPage: 'the_form_processor',
 };
 
 export default pathnames;
