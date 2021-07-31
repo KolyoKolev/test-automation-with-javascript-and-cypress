fb168b216e96e150931305baaca7b9c810e3c628
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-07-31 08:48:11 +0300

    capitalize the pathnames consts

--- cypress/constants/pathnames.js
+++ cypress/constants/pathnames.js
@@ -1,10 +1,10 @@
-const pathnames = {
-  indexPage: 'index',
-  basicWebPage: 'basic-web-page-test',
-  attributesPage: 'attributes-test',
-  tablePage: 'tag/table',
-  basicJavaScriptValidationPage: 'basic-javascript-validation-test',
-  theFormProcessorPage: 'the_form_processor',
+const PATHNAMES = {
+  INDEX_PAGE: 'index',
+  BASIC_WEB_PAGE: 'basic-web-page-test',
+  ATTRIBUTES_PAGE: 'attributes-test',
+  TABLE_PAGE: 'tag/table',
+  BASIC_JAVA_SCRIPT_VALIDATION_PAGE: 'basic-javascript-validation-test',
+  THE_FORM_PROCESSOR_PAGE: 'the_form_processor',
 };
 
-export default pathnames;
+export default PATHNAMES;
