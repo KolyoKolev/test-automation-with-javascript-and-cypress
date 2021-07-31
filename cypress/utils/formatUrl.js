40178f404f8c5ba94647e155bf81569931e17f4e
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-07-31 08:48:32 +0300

    refactor the formatUrl util function

--- cypress/utils/formatUrl.js
+++ cypress/utils/formatUrl.js
@@ -1,14 +1,12 @@
-import { BASE_URL, HTML, PHP } from '../constants';
+const formatUrl = (baseUrl, path, fileType) => {
+  const FILE_TYPES = {
+    HTML: 'html',
+    PHP: 'php',
+  };
 
-const formatUrl = (path, fileType) => {
-  switch (fileType) {
-    case HTML:
-      return `${BASE_URL}${path}.${HTML}`;
-    case PHP:
-      return `${BASE_URL}${path}.${PHP}`;
-    default:
-      return new Error(cy.log('Check the url format. Must be wrong!'));
-  }
+  return FILE_TYPES[fileType]
+    ? `${baseUrl}${path}.${FILE_TYPES[fileType]}`
+    : new Error(cy.log('Check the url format. Must be wrong!'));
 };
 
 export default formatUrl;
