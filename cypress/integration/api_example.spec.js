cce5144ae65620caf1d8d53f07db7070a0711cf3
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-08-01 07:45:02 +0300

    update the api example spec

--- cypress/integration/api_example.spec.js
+++ cypress/integration/api_example.spec.js
@@ -1,14 +1,16 @@
+import { REQ_RES_APP } from '../constants/index';
+
 describe('reqres api', () => {
   it('should verify the creation of new user', () => {
     cy.request({
       method: 'POST',
-      url: '/users',
+      url: `${REQ_RES_APP.BASE_URL}/users`,
       body: {
         id: 7,
         email: 'test@reqres.in',
         first_name: 'Test',
         last_name: 'Testov',
-        avatar: 'https://reqres.in/img/faces/123-image.jpg',
+        avatar: `${REQ_RES_APP.BASE_URL}/img/faces/123-image.jpg`,
       },
     }).then((res) => {
       expect(res.body.id).to.eq(7);
@@ -20,7 +22,7 @@ describe('reqres api', () => {
   });
 
   it('should verify the content-type header', () => {
-    cy.request('/users')
+    cy.request(`${REQ_RES_APP.BASE_URL}/users`)
       .its('headers')
       .its('content-type')
       .should('include', 'application/json');
