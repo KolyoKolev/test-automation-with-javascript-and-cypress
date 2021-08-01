771eb3485cb79c86240e0ff7aa25b5772537027e
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-08-01 06:49:38 +0300

    refactor the constants structure

--- cypress/constants/baseUrls.js
+++
@@ -1,7 +0,0 @@
-const BASE_URLS = {
-  THE_INTERNET_APP: 'https://the-internet.herokuapp.com/',
-  SELENIUM_TEST_PAGES_APP: 'https://testpages.herokuapp.com/styled/',
-  REQ_RES_APP: 'https://reqres.in/api',
-};
-
-export default BASE_URLS;
--- cypress/constants/index.js
+++ cypress/constants/index.js
@@ -1,2 +1,22 @@
 export const HTML = 'HTML';
 export const PHP = 'PHP';
+
+export const SELENIUM_TEST_PAGES_APP = {
+  BASE_URL: 'https://testpages.herokuapp.com/styled/',
+  PATHNAMES: {
+    INDEX_PAGE: 'index',
+    BASIC_WEB_PAGE: 'basic-web-page-test',
+    ATTRIBUTES_PAGE: 'attributes-test',
+    TABLE_PAGE: 'tag/table',
+    BASIC_JAVA_SCRIPT_VALIDATION_PAGE: 'basic-javascript-validation-test',
+    THE_FORM_PROCESSOR_PAGE: 'the_form_processor',
+  },
+};
+
+export const THE_INTERNET_APP = {
+  BASE_URL: 'https://the-internet.herokuapp.com/',
+};
+
+export const REQ_RES_APP = {
+  BASE_URL: 'https://reqres.in/api',
+};
