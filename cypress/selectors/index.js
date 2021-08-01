f676290137ee6bf62cfd195d66ff2b22db5f4296
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-08-01 07:44:25 +0300

    refactor the selectors

---
+++ cypress/selectors/index.js
@@ -0,0 +1,31 @@
+export const SELENIUM_TEST_PAGES_APP = {
+  BASIC_TEST_PAGE: {
+    PAGE_BODY: '.page-body',
+    NAVIGATION_INDEX_LINK: '.page-navigation a',
+    PAGE_H1_HEADING: 'h1',
+    PARAGRAPH: 'p',
+    EXPLANATION_SECTION: '.explanation',
+    FOOTER: '.page-footer',
+  },
+  ATTRIBUTES_TEST_PAGE: {
+    PARAGRAPH_WITH_DYNAMIC_ATTRIBUTES: '#jsattributes',
+    BUTTON: 'button.styled-click-button',
+  },
+  TABLE_TEST_PAGE: {
+    TABLE_SECTION: '#mytable',
+    TABLE_ROW: 'tr',
+  },
+  BASIC_JAVASCRIPT_VALIDATION_TEST_PAGE: {
+    INPUT_FIELD: '#lteq30',
+    INPUT_SUBMIT: 'input.styled-click-button',
+  },
+  THE_FORM_PROCESSOR_TEST_PAGE: {
+    SUBMITTED_VALUE_FIELD: '#_valuevalue',
+  },
+};
+
+export const THE_INTERNET_APP = {
+  FILE_UPLOAD: '#file-upload',
+  UPLOAD_BTN: '#file-submit',
+  DRAG_AND_DROP: '#drag-drop-upload',
+};
