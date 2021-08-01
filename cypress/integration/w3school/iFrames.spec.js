f3912ef77914f9984ff5f27261a3e602a9a1707a
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-08-01 08:14:43 +0300

    update iframe spec

--- cypress/integration/w3school/iFrames.spec.js
+++ cypress/integration/w3school/iFrames.spec.js
@@ -1,14 +1,20 @@
+import { W3SCHOOL_APP } from '../../constants';
+import * as selectors from '../../selectors';
+
 Cypress.Commands.add('acceptTermAndConditions', () => {
-  cy.get('#accept-choices').click();
+  cy.get(selectors.W3SCHOOL_APP.ACCEPT_TERMS_AND_CONDITIONS).click();
 });
 
 Cypress.Commands.add('getIFrame', () => {
-  cy.get('#iframeResult').its('0.contentDocument').its('body').then(cy.wrap);
+  cy.get(selectors.W3SCHOOL_APP.IFRAME)
+    .its('0.contentDocument')
+    .its('body')
+    .then(cy.wrap);
 });
 
 describe('handling iframes', () => {
   it('should verify the h1 of an iframe', () => {
-    cy.visit('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe')
+    cy.visit(`${W3SCHOOL_APP.BASE_URL}${W3SCHOOL_APP.PATHNAMES.IFRAME}`)
       .acceptTermAndConditions()
       .getIFrame()
       .find('h1')
