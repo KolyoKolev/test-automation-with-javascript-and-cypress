5e7867225106e84137b65718b273071659120c8a
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-09-13 16:13:11 +0300

    add spec for client side form validation

---
+++ cypress/integration/client_side_form_validation/client-side-form-validation.spec.js
@@ -0,0 +1,20 @@
+/// <reference types='cypress'/>
+
+const FORM_TEST_PAGE = './pages/form/index.html';
+
+const SELECTORS = {
+  inputField1: '[data-test="test-input-1"]',
+  inputField2: '[data-test="test-input-2"]',
+  inputField3: '[data-test="test-input-3"]',
+  submitBtn: '[data-test="submit-btn"]',
+};
+
+describe('Client side form validation', () => {
+  before('load form test page', () => {
+    cy.visit(FORM_TEST_PAGE);
+  });
+
+  it('should check the number of invalid inputs on initial load', () => {
+    cy.get('input:invalid').should('have.length', 3);
+  });
+});
