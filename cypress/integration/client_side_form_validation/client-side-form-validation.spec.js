9026d2c0ac68dca7d0d227ee2308dc62d1775b01
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-09-13 18:27:13 +0300

    add new test

--- cypress/integration/client_side_form_validation/client-side-form-validation.spec.js
+++ cypress/integration/client_side_form_validation/client-side-form-validation.spec.js
@@ -31,10 +31,23 @@ describe('Client side form validation', () => {
       .should('be.true');
   });
 
-  it('should check the validation error message of the first input', () => {
+  it('should check the presence of validation error message of the first input when field is left empty', () => {
     cy.get(SELECTORS.submitBtn).click();
     cy.get(SELECTORS.inputField1)
       .invoke('prop', 'validationMessage')
       .should('eq', 'Test field 1 should not be left empty');
+  });
+
+  it('should check the lack of validation error message of the first input when text is typed in the field ', () => {
+    cy.get(SELECTORS.inputField1)
+      .type('test')
+      .get(SELECTORS.submitBtn)
+      .click()
+      .get(SELECTORS.inputField1)
+      .invoke('prop', 'validationMessage')
+      .should('eq', '')
+      .get(SELECTORS.inputField2)
+      .invoke('prop', 'validationMessage')
+      .should('eq', 'Test field 2 should not be left empty');
   });
 });
